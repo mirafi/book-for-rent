2fa99330c0e545cfb591d53557f09515ccda4c7c
mi-rafi <rafi101010@gmail.com>
2019-08-17 16:45:42 +0600

    First commit

---
+++ src/main/java/self/mirafi/tdd/demo/bookforrent/BookForRentApplication.java
@@ -0,0 +1,13 @@
+package self.mirafi.tdd.demo.bookforrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookForRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookForRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/self/mirafi/tdd/demo/bookforrent/BookForRentApplicationTests.java
@@ -0,0 +1,16 @@
+package self.mirafi.tdd.demo.bookforrent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookForRentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
